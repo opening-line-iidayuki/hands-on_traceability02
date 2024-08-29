48d262cb69a937ea48be40f984dc189e3ced9492
opening-line_yi <iida_y@opening-line.co.jp>
2024-08-29 14:50:26 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: false,
+  },
   reactStrictMode: true,
 };
-
-export default nextConfig;
+module.exports = nextConfig;
